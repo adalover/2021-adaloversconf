81c26a53f81caf559c53ab8c2b51f02172a99b9b
yodralopez <yodralopez@gmail.com>
2021-08-01 18:27:20 +0100

    add code of conduct

--- lib/constants.ts
+++ lib/constants.ts
@@ -38,7 +38,7 @@ export const LEGAL_URL = process.env.NEXT_PUBLIC_PRIVACY_POLICY_URL;
 export const COPYRIGHT_HOLDER = process.env.NEXT_PUBLIC_COPYRIGHT_HOLDER;
 
 export const CODE_OF_CONDUCT =
-  'https://www.notion.so/vercel/Code-of-Conduct-Example-7ddd8d0e9c354bb597a0faed87310a78';
+  'https://creative-glasses-5eb.notion.site/C-digo-de-conducta-d99cf9bc2b294b088856d8125821f1b6';
 export const REPO = 'https://github.com/adalover/2021-adaloversconf';
 export const SAMPLE_TICKET_NUMBER = 1234;
 export const NAVIGATION = [
