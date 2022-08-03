398a69340e401f8aece58b59f9c1b97ef0077eb3
Noemi Delgado <noemidsc@gmail.com>
2022-08-03 13:55:17 +0100

    Refactor principal buttons

---
+++ components/buttons/buttonSecondary.tsx
@@ -0,0 +1,38 @@
+import styled from '@emotion/styled';
+
+interface Props {
+  text: string,
+  action: () => void,
+}
+
+const SecondaryButton = styled.button`
+  border-radius: var(--space-2x);
+  border: 1px solid var(--white);
+  color: var(--white);
+  background: var(--secondary-color);
+  cursor: pointer;
+  font-family: inherit;
+  font-size: var(--text-md);
+  letter-spacing: -0.02em;
+  font-weight: 500;
+  outline: none;
+  transition: background-color 0.2s ease;
+  padding: var(--space-6x);
+  height: 55px;
+  width: 100%;
+  max-width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  :hover {
+    background: var(--white);
+    color: var(--secondary-color);
+    border: 1px solid var(--secondary-color);
+}
+`
+
+export const ButtonSecondary = ({ text, action }: Props) =>
+  <SecondaryButton onClick={action}>
+    {text}
+  </SecondaryButton> 
